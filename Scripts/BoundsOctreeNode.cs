7f886f5fdabd41ed0ae24270a2fe95d0dd6a7403
Nashnir <nashnir@gmail.com>
2018-08-28 02:21:43 -0700

    no message

--- Scripts/BoundsOctreeNode.cs
+++ Scripts/BoundsOctreeNode.cs
@@ -36,7 +36,7 @@ public class BoundsOctreeNode<T> {
 	const int numObjectsAllowed = 8;
 
 	// An object in the octree
-	class OctreeObject {
+	struct OctreeObject {
 		public T Obj;
 		public Bounds Bounds;
 	}
@@ -86,7 +86,8 @@ public class BoundsOctreeNode<T> {
 		}
 
 		if (!removed && children != null) {
-			for (int i = 0; i < 8; i++) {
+			for (int i = 0; i < 8; i++) 
+			{
 				removed = children[i].Remove(obj);
 				if (removed) break;
 			}
@@ -499,8 +500,10 @@ public class BoundsOctreeNode<T> {
 		bool removed = false;
 
 		for (int i = 0; i < objects.Count; i++) {
-			if (objects[i].Obj.Equals(obj)) {
-				removed = objects.Remove(objects[i]);
+			if (objects[i].Obj.Equals(obj))
+			{
+				removed = true;
+				objects.RemoveAt(i);
 				break;
 			}
 		}
@@ -598,7 +601,7 @@ public class BoundsOctreeNode<T> {
 	/// Checks if this node or anything below it has something in it.
 	/// </summary>
 	/// <returns>True if this node or any of its children, grandchildren etc have something in them</returns>
-	public bool HasAnyObjects() {
+	internal bool HasAnyObjects() {
 		if (objects.Count > 0) return true;
 
 		if (children != null) {
